081e7ed553adc5959d5617e74af502e07a8b3593
FavelLooking <intwestpol@gmail.com>
2024-10-03 22:36:53 +0400

    feat: add list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { promises as fs } from "fs";
+import path from "path";
+import * as url from "url";
+
 const list = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+  const folderPath = path.resolve(__dirname, "files");
+
+  try {
+    await fs.access(folderPath);
+    const directory = await fs.readdir(folderPath);
+    console.log(...directory);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await list();
