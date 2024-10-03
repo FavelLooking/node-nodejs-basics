841e601dd8674a15ab2ac8fc97f1cdb2170d2bde
FavelLooking <intwestpol@gmail.com>
2024-10-03 23:09:49 +0400

    feat: add args env task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const modifiedArgs = args.map((arg) => arg.replace("--", ""));
+  for (let i = 0; i < modifiedArgs.length; i += 2) {
+    console.log(`${modifiedArgs[i]} is ${modifiedArgs[i + 1]}`);
+  }
 };
 
 parseArgs();
