0e953528007ba9af3cc5bb3a8243b2733b9bfd96
FavelLooking <intwestpol@gmail.com>
2024-10-05 17:49:05 +0400

    feat: solve streams read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import path from "path";
+import * as url from "url";
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+  const filePath = path.resolve(__dirname, "files", "fileToRead.txt");
+
+  const fileStream = fs.createReadStream(filePath);
+
+  fileStream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
+  fileStream.on("error", (err) => {
+    console.error("Error:", err);
+  });
 };
 
 await read();
