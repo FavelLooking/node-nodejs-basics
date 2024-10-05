1aa8f448b599f7ab06e4f68d7efbead8d9955410
FavelLooking <intwestpol@gmail.com>
2024-10-05 18:45:30 +0400

    feat: solve streams write task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import fs from "fs";
+import path from "path";
+import * as url from "url";
+
 const write = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+  const filePath = path.resolve(__dirname, "files", "fileToWrite.txt");
+
+  const writeStream = fs.createWriteStream(filePath);
+
+  process.stdin.pipe(writeStream);
 };
 
 await write();
