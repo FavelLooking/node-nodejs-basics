7a843895e79de3f5c6ee7765ecfcec0de5a3ccd8
FavelLooking <intwestpol@gmail.com>
2024-10-05 21:12:26 +0400

    feat: solve compress and decompress tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,30 @@
+import fs from "fs";
+import path from "path";
+import * as url from "url";
+import { createGzip } from "zlib";
+import { pipeline } from "stream";
+
 const compress = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+  const filePath = path.resolve(__dirname, "files", "fileToCompress.txt");
+  const newFilePath = path.resolve(__dirname, "files", "archive.gz");
+
+  const gzip = createGzip();
+  const source = fs.createReadStream(filePath);
+  const destination = fs.createWriteStream(newFilePath);
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      console.error("Error:", err);
+      process.exitCode = 1;
+    } else {
+      console.log("success");
+      fs.unlink(filePath, (unlinkErr) => {
+        if (unlinkErr) {
+          console.error("Error:", unlinkErr);
+        }
+      });
+    }
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,35 @@
+import fs from "fs";
+import path from "path";
+import * as url from "url";
+import { createGunzip } from "zlib";
+import { pipeline } from "stream";
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+  const compressedFilePath = path.resolve(__dirname, "files", "archive.gz");
+  const originalFilePath = path.resolve(
+    __dirname,
+    "files",
+    "fileToCompress.txt",
+  );
+
+  const gunzip = createGunzip();
+  const source = fs.createReadStream(compressedFilePath);
+  const destination = fs.createWriteStream(originalFilePath);
+
+  pipeline(source, gunzip, destination, (err) => {
+    if (err) {
+      console.error("Error:", err);
+      process.exitCode = 1;
+    } else {
+      console.log("Files were decompressed");
+      fs.unlink(compressedFilePath, (unlinkErr) => {
+        if (unlinkErr) {
+          console.error("Error:", unlinkErr);
+        }
+      });
+    }
+  });
 };
 
 await decompress();
