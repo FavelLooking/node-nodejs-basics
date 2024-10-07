269f9e5d9b39559e0122cb5fbb8babfca0e90423
FavelLooking <intwestpol@gmail.com>
2024-10-07 15:13:15 +0400

    fix streams read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -6,10 +6,13 @@ const read = async () => {
   const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
   const filePath = path.resolve(__dirname, "files", "fileToRead.txt");
 
-  const fileStream = fs.createReadStream(filePath);
+  const fileStream = fs.createReadStream(filePath, { encoding: "utf-8" });
 
   fileStream.on("data", (chunk) => {
     process.stdout.write(chunk);
+    process.stdin.once("data", () => {
+      process.exit();
+    });
   });
   fileStream.on("error", (err) => {
     console.error("Error:", err);
